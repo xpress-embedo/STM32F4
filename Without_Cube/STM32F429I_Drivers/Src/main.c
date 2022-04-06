d0db8013361d2b3a06c0e7c1874151d2c780d221
xpress_embedo <laboratory.embedded@gmail.com>
2022-04-07 01:45:16 +0530

    * Drivers are created as submodule, so that can be used as shared project.

--- Without_Cube/STM32F429I_Drivers/Src/main.c
+++ Without_Cube/STM32F429I_Drivers/Src/main.c
@@ -17,6 +17,7 @@
  */
 
 #include <stdint.h>
+#include "stm32f429xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
