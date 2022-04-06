4bef1c6b43b525ab90150d91f5d0dc2ec40ad2fd
xpress_embedo <laboratory.embedded@gmail.com>
2022-04-07 03:07:16 +0530

    * submodule updated (GPIO Driver Development is in progress)

--- Without_Cube/STM32F429I_Drivers/Src/main.c
+++ Without_Cube/STM32F429I_Drivers/Src/main.c
@@ -17,7 +17,7 @@
  */
 
 #include <stdint.h>
-#include "stm32f429xx.h"
+#include "stm32f429xx_gpio.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
