e11faef027a3d575f842473e48d85a7cbbb12962
xpress-embedo <laboratory.embedded@gmail.com>
2022-11-05 10:09:19 +0100

    LVGL Sample: Frame Buffer is in External SDRAM * SDRAM Libraries are enabled but initialization and other stuff is done in tft modules, this part is not generated by CubeMX

--- LVGL_Sample/Core/Src/main.c
+++ LVGL_Sample/Core/Src/main.c
@@ -96,11 +96,11 @@ int main(void)
   // rotate the display
   lv_disp_set_rotation( lv_disp_get_default(), LV_DISP_ROT_270 );
 
-  // lv_example_get_started_1();
+  lv_example_get_started_1();
   // lv_example_label_1();
   // lv_example_btn_1();
   // lv_example_scroll_1();
-  lv_example_anim_1();
+  // lv_example_anim_1();
   
   // RGB Mixer User Interface
   // rgb_mixer_created_ui();
