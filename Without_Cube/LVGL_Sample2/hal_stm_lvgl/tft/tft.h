5750efae18317cdc348253e85446ba54b01dacaf
xpress-embedo <laboratory.embedded@gmail.com>
2022-12-13 20:53:00 +0100

    LVGL_Sample2 Application updated to use external SDRAM for frame buffer * this reduces the RAM consumption to 20% only from 97% because now the frame buffer are in external SDRAM * CubeMX generated FMC SDRAM configuration is not used, but the CubeMX project is updated in such a way that the same config will be generated if used. * TFT_EXT_FB is set to 1 to use external SDRAM frame buffer

--- Without_Cube/LVGL_Sample2/hal_stm_lvgl/tft/tft.h
+++ Without_Cube/LVGL_Sample2/hal_stm_lvgl/tft/tft.h
@@ -18,9 +18,9 @@
 #define TFT_HOR_RES 240
 #define TFT_VER_RES 320
 
-//#define TFT_EXT_FB		1		/*Frame buffer is located into an external SDRAM*/
+#define TFT_EXT_FB		1		/*Frame buffer is located into an external SDRAM*/
 //#define TFT_USE_GPU		1		/*Enable hardware accelerator*/
-#define TFT_EXT_FB    0   /*Frame buffer is located into an internal RAM*/
+// #define TFT_EXT_FB    0   /*Frame buffer is located into an internal RAM*/
 #define TFT_USE_GPU   0   /*Disable hardware accelerator*/
 
 /**********************
